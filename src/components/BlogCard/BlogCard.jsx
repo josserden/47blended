86f087e08896ce156a63eef418c2ad6bf1bc8371
ViktoriaFomenko <aktavia@gmail.com>
2022-07-23 15:18:59 +0300

    task 1

--- src/components/BlogCard/BlogCard.jsx
+++ src/components/BlogCard/BlogCard.jsx
@@ -1,3 +1,4 @@
+import { formatDateToNow } from 'helpers/formatDateToNow';
 import {
   Card,
   CardBody,
@@ -14,6 +15,34 @@ import {
   Date,
 } from './BlogCard.styled';
 
-export const BlogCard = () => {
-  return <div>BlogCard</div>;
+export const BlogCard = ({
+  poster,
+  tag,
+  title,
+  description,
+  avatar,
+  postedAt,
+  name,
+}) => {
+  return (
+    <Card>
+      <CardHeader>
+        <CardPoster src={poster} alt={tag} />
+      </CardHeader>
+      <CardBody>
+        <Tag>{tag}</Tag>
+        <CardTitle>{title}</CardTitle>
+        <CardText>{description}</CardText>
+      </CardBody>
+      <CardFooter>
+        <UserBox>
+          <Avatar src={avatar} alt={name} />
+          <UserInfo>
+            <UserName>{name}</UserName>
+            <Date>{formatDateToNow(postedAt)}</Date>
+          </UserInfo>
+        </UserBox>
+      </CardFooter>
+    </Card>
+  );
 };
---
+++ src/helpers/formatDateToNow.js
@@ -0,0 +1,4 @@
+import { formatDistanceToNow } from 'date-fns';
+export const formatDateToNow = date => {
+  return formatDistanceToNow(new Date(date), { addSuffix: true });
+};
